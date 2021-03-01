a88c85abd55d17623359771723167dbafa1b3358
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-02-28 21:36:21 -0500

    working on sub template

--- utilities.py
+++ utilities.py
@@ -1,8 +1,5 @@
-import pandas as pd
 from utils.databaser import MakeMongo
 
-
-# uncomment the next two lines to generate fake data
 from utils.make_data import *
 make_form_data()
 
@@ -11,9 +8,10 @@ df = pd.read_csv("data/observation_forms_2.csv")
 # remove any invalid characters from df cols
 df.columns = df.columns.str.replace(".", "_")
 
-temperament = ["The judge was professional", "The judge was compassionate", "The judge was respectful", "The judge was patient"]
-communication = []
-
+temperament = ["The judge was professional"
+              , "The judge was compassionate"
+              , "The judge was respectful"
+              , "The judge was patient"]
 
 df[temperament] = df[temperament].replace({'Strongly Agree':5
                                            ,'Agree':4
@@ -27,5 +25,5 @@ monger = MakeMongo()
 # before running next, configure env variables for database access
 monger.insert_df(collection='hci', df=df)
 
-
+communication = []
 
