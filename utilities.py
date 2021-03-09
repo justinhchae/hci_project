f76b4bdad7d7a5d95965fe1ca61f75c91937e2ea
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-09 14:02:44 -0500

    update aggregate temperament score

--- utilities.py
+++ utilities.py
@@ -6,7 +6,9 @@ make_form_data()
 df = pd.read_csv("data/observation_forms_2.csv")
 
 # remove any invalid characters from df cols
-df.columns = df.columns.str.replace(".", "_")
+df.columns = df.columns.str.replace(".", "_", regex=True)
+
+df = df.dropna(axis='columns', how='all')
 
 temperament = ["The judge was professional"
               , "The judge was compassionate"
@@ -25,5 +27,19 @@ monger = MakeMongo()
 # before running next, configure env variables for database access
 monger.insert_df(collection='hci', df=df)
 
-communication = []
+# df = pd.DataFrame()
+# df = df.rename(columns={'Judge Name 1':'Judge Name'})
+# df = df.dropna(subset=['Judge Name'])
 
+df = df.groupby('Judge Name 1')[temperament].agg('mean')
+
+df = df.agg(['mean', 'std', 'min', 'max'], axis='columns').reset_index()
+df = df.rename(columns={'index':'Judge Name 1'
+                       , 'mean': 'Temperament Score'})
+
+
+print(df['Temperament Score'])
+
+# create a databaser object for mongoDB
+monger = MakeMongo()
+monger.insert_df(collection='agg_temperament_scores', df=df)
